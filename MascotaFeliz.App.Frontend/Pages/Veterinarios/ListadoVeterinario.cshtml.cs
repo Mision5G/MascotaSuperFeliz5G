808fd73b08005e46605ea4e48899a3a6d191cd53
WilliamRinconReyes <williamrinconreyes@gmail.com>
2022-09-20 12:22:21 -0500

    a

--- MascotaFeliz.App.Frontend/Pages/Veterinarios/ListadoVeterinario.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Veterinarios/ListadoVeterinario.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
 namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListadoVeterinarioModel : PageModel
     {
+        private readonly IRepositorioVeterinario _repoVeterinario;
+        public IEnumerable<Veterinario> listaDuenos {get;set;}
+        
+        public ListaVeterinarioModel()
+
+        {
+            this._repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+        }
+
         public void OnGet()
         {
+            ListadoVeterinario = _repoVeterinario.GetAllVeterinarios();
         }
     }
 }
