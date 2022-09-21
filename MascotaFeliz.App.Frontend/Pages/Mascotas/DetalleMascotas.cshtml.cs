110b1240e1f6055d3b97a3225e6b86ce190b9e5b
Mision5G <misiontic.5g@gmail.com>
2022-09-20 20:13:43 -0500

    Aporte hoy

--- MascotaFeliz.App.Frontend/Pages/Mascotas/DetalleMascotas.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Mascotas/DetalleMascotas.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
 namespace MascotaFeliz.App.Frontend.Pages
 {
     public class DetalleMascotasModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMascota _repoMascota;
+        public Mascota mascota {get;set;}
+        public DetalleMascotasModel()
         {
+            this._repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+        }
+        public IActionResult OnGet(int mascotaId)
+        {
+            mascota = _repoMascota.GetMascota(mascotaId);
+            if (mascota == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
